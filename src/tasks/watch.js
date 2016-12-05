4d78a803a03036b14c81538e72fdb8da6d924b33
apkawa <apkawa@gmail.com>
2016-12-05 17:06:55 +0300

    update watch

--- src/tasks/watch.js
+++ src/tasks/watch.js
@@ -1,18 +1,32 @@
 'use strict';
+var _watch = require('gulp-watch');
+var batch = require('gulp-batch');
+import runSequence from 'run-sequence';
+
+
 export default function (gulp, config) {
     var browserSyncOptions = config.browserSync;
     var browserSync = browserSyncOptions.browserSync;
+
+    function watch(glob, tasks) {
+        return _watch(glob, batch((events, done) => {
+            runSequence.use(gulp)(
+                ...[...tasks, done
+                ]
+            )
+        }))
+
+    }
 
     const {project: {path: {app: APP_PATH}}} = config;
     /*
      * Default & Watch tasks
      */
     gulp.task('watch', ['default'], function () {
-        gulp.watch(APP_PATH.css + '**/*', ['css', browserSync.reload]);
-        gulp.watch(APP_PATH.template + '**/*', ['templates',]);
-        gulp.watch(APP_PATH.template_context + '**/*', ['templates',]);
-        gulp.watch(APP_PATH.public, ['public', browserSync.reload]);
-        //gulp.watch(APP_PATH.js + '**/*', []);
+        watch(APP_PATH.css + '**/*[^_]', ['css', browserSync.reload]);
+        watch(APP_PATH.template + '**/*[^_]', ['templates',]);
+        watch(APP_PATH.template_context + '**/*[^_]', ['templates',]);
+        watch(APP_PATH.public, ['public', browserSync.reload]);
     });
 
     gulp.task('watch:webpack', ['watch', 'webpack:watch']);
